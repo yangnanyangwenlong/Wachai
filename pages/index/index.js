0a45e467a1b0725782a4f4c1a5948f56b4926193
yangnan <2589495129@qq.com>
2020-11-16 19:53:48 +0800

    页面处理

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,7 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
+    name: ' yangnan',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
@@ -21,7 +22,20 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    wx.request({
+        url: 'http://2004shop.com/api/test', //仅为示例，并非真实的接口地址
+        data: {
+          x: 'xxx',
+          y: 'yy'
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success (res) {
+          goods_name:res.data.goods_name
+        }
+      })
+      } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -48,7 +62,8 @@ Page({
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      hasUserInfo: true,
+      name2: '111'
     })
   }
 })
